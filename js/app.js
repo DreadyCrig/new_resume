54c99d9b762a5b4aee545cb2decbf53f8ac68c33
Shawn Crigger <shawn@southerntidemedia.com>
2018-01-14 11:53:55 -0500

    Syncing work that I forgot doh

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+
+
+function init_imgs()
+{
+	var imgDefer = document.getElementsByTagName('img');
+	for (var i=0; i<imgDefer.length; i++) {
+		if(imgDefer[i].getAttribute('data-src')) {
+			imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+		}
+	}
+}
+window.onload = init_imgs;
+
 $(document).foundation();
